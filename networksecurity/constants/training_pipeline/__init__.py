0614aebf4023944d32b8ce5973f3f6876adf1308
Nishan-k <karki.nishann@gmail.com>
2025-11-26 06:36:54 +0100

    work in progress on config_entity.py

---
+++ networksecurity/constants/training_pipeline/__init__.py
@@ -0,0 +1,25 @@
+import os
+import numpy as np
+import pandas as pd
+
+
+###############################################
+# Constant variables for the Training Pipeline:
+###############################################
+TARGET_COLUMN = "Result"
+PIPELINE_NAME: str = "NetworkSecurity"
+ARTIFACT_DIR: str = "Artifacts"
+FILE_NAME: str = "phisingData.csv"
+TRAIN_FILE_NAME: str = "train.csv"
+TEST_FILE_NAME: str = "test.csv"
+
+
+############################################
+# Constant variables for the Data Ingestion:
+############################################
+DATA_INGESTION_COLLECTION_NAME: str = "PhisingData"
+DATA_INGESTION_DATABASE_NAME: str = "Nishan-AI-Engineer"
+DATA_INGESTION_DIR_NAME: str = "data_ingestion"
+DATA_INGESTION_FEATURE_STORE_DIR: str = "feature_store"
+DATA_INGESTION_INGESTED_DIR: str = "ingested"
+DATA_INGESTION_TRAIN_TEST_SPLIT_RATIO:float = 0.2
